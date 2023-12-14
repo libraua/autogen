d4d778ccf2a7802628e8e270132934e52f0e9d6f
Li Jiang <bnujli@gmail.com>
2023-12-14 15:57:18 +0800

    Fix missing pydash

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setuptools.setup(
         "teachable": ["chromadb"],
         "lmm": ["replicate", "pillow"],
         "graphs": ["networkx~=3.2.1", "matplotlib~=3.8.1"],
-        "gemini": ["google-generativeai"],
+        "gemini": ["google-generativeai", "pydash"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
